8ec59bbf05e25896506ef0a580934870753b6769
Vander <jeromeevander13@gmail.com>
2026-02-12 13:55:11 +0800

    adding seeders

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -137,7 +137,7 @@
 
     <div class="text-center mt-12 mb-10">
         <h1 class="text-6xl md:text-7xl font-black uppercase italic leading-[0.8] tracking-tighter">
-             THIS OUR<br>
+             THIS  IS OUR<br>
             <span class="text-red-600 not-italic">TEAM</span>
         </h1>
         <p class="uppercase text-[9px] tracking-[0.4em] text-gray-400 font-bold mt-6">
